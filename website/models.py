9c0af5a45c910aa614ce6b3e084b68ee86a8cc69
bibin thomas <82058754+bibinthomas123@users.noreply.github.com>
2022-07-14 21:11:04 +0530

    update:Added more commits

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+# the schema of the database 
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)

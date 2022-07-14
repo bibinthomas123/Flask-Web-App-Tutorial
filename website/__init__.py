8f83bc8b327bf4c3f0406833c9eb465165e47164
bibin thomas <82058754+bibinthomas123@users.noreply.github.com>
2022-07-14 21:03:01 +0530

    Update:Added more commits

--- website/__init__.py
+++ website/__init__.py
@@ -5,18 +5,18 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-
+# creates a database with name "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # pointing the database
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/') #  registering blueprint to the app
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
@@ -34,7 +34,7 @@ def create_app():
     return app
 
 
-def create_database(app):
+def create_database(app): # this function checks if the database exists or not else the database will be created 
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
